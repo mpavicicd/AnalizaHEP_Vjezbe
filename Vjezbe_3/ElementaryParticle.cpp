3af45469db570871f8c888831cabbd73783062bf
mpavicicd <mpavicicd@pmfst.hr>
2020-10-19 22:24:57 +0200

    Vjezbe_3

--- Vjezbe_3/ElementaryParticle.cpp
+++ Vjezbe_3/ElementaryParticle.cpp
@@ -19,19 +19,46 @@
 		en = sqrt(px*px + py*py + pz*pz + masacestice*masacestice);
 	}
 	void ElementaryParticle::printTrans() { //racun transverzalne kol gibanja
-		cout << "transverzalna kolicina gibanja: " << sqrt(px*px + py*py) << endl;
+		cout << "p_T = " << sqrt(px*px + py*py) << endl;
 	}
-	void ElementaryParticle::bosonDecay(ElementaryParticle obj1, ElementaryParticle obj2) {
+	void ElementaryParticle::bosonDecay(ElementaryParticle &obj1, ElementaryParticle &obj2) {
 		int r;
+		double px1, px2, py1, py2, pz1, pz2;
 		if(ifbozon!=1) //a - ako nije bozon
 			cout << "Raspad nije moguc." << endl;
-		r = rand() % 1000;
-		if(r < 214) //21.4% slucajeva raspad na W bozone (0-213)
-			cout << "Raspad na W bozone." << endl;
-		else if(r >= 214 && r < 278) //6.4% slucajeva raspad na tau leptone (214-277)
-			cout << "Raspad na tau leptone." << endl;
-		else if(r >= 278 && r < 304) //2.6% slucajeva raspad na tau leptone (278-303)
-			cout << "Raspad na Z bozone." << endl;
-		else
-			cout << "Raspad na b kvarkove." << endl;
+		else {
+			r = rand() % 1000;
+			if(r < 214){ //21.4% slucajeva raspad na W bozone (0-213)
+				cout << "Raspad na W bozone." << endl;
+				obj1.imecestice = "W bozon";
+				obj2.imecestice = "W bozon";
+				//trebalo bi dodati postavljanje mase i ifbozon
+			}
+			else if(r >= 214 && r < 278){ //6.4% slucajeva raspad na tau leptone (214-277)
+				cout << "Raspad na tau leptone." << endl;
+				obj1.imecestice = "tau lepton";
+				obj2.imecestice = "tau lepton";
+				//trebalo bi dodati postavljanje mase i ifbozon
+			}
+			else if(r >= 278 && r < 304){ //2.6% slucajeva raspad na tau leptone (278-303)
+				cout << "Raspad na Z bozone." << endl;
+				obj1.imecestice = "Z bozon";
+				obj2.imecestice = "Z bozon";
+				//trebalo bi dodati postavljanje mase i ifbozon
+			}
+			else{
+				cout << "Raspad na b kvarkove." << endl;
+				obj1.imecestice = "b kvark";
+				obj2.imecestice = "b kvark";
+				//trebalo bi dodati postavljanje mase i ifbozon
+			}
+			px1 = px * (double)(rand() % 1000)/1000;
+			px2 = px - px1;
+			py1 = py * (double)(rand() % 1000)/1000;
+			py2 = py - py1;
+			pz1 = pz * (double)(rand() % 1000)/1000;
+			pz2 = pz - pz1;
+			obj1.energija(px1,py1,pz1);
+			obj2.energija(px2,py2,pz2);
+		}
 	}
--- Vjezbe_3/analyzer.cpp
+++ Vjezbe_3/analyzer.cpp
@@ -2,13 +2,17 @@
 
 int main () {
   srand((int)time(0));
-  ElementaryParticle *pok1, *pok2;
+  ElementaryParticle *pok1, *pok2, *pok3;
   pok1 = new ElementaryParticle(125.18,"Higgsov bozon",true);
-  pok2 = new ElementaryParticle(1,"Druga cestica",false);
+  pok2 = new ElementaryParticle();
+  pok3 = new ElementaryParticle();
   //pok1->printInfo();
-  pok1->energija(1,2,3);
+  pok1->energija(1,1,1);
   pok1->printTrans();
-  pok1->bosonDecay(*pok1, *pok2);
+  pok1->bosonDecay(*pok2, *pok3);
+  cout << "4-vektor prve cestice (E, px, py, pz): (" << pok2->en <<", "<< pok2->px <<", "<< pok2->py <<", "<< pok2->pz <<")"<< endl;
+  cout << "4-vektor druge cestice (E, px, py, pz): (" << pok3->en <<", "<< pok3->px <<", "<< pok3->py <<", "<< pok3->pz <<")"<< endl;
+  cout << "Nastala cestica: " << pok2->imecestice << endl;
   delete pok1;
   delete pok2;
   return 0;
