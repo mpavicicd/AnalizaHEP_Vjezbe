ed925ae0a916df0732e79c17be78dd5753e51626
mpavicicd <mpavicicd@pmfst.hr>
2020-10-26 20:55:43 +0100

    Vjezbe_4

---
+++ Vjezbe_4/Analyzer.h
@@ -0,0 +1,15 @@
+#include <cstdlib> 
+#include <ctime> 
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+
+
+using namespace std;
+
+class Analyzer{
+	public:
+		Analyzer(); //poziv konstruktora
+		void readFile();
+};
