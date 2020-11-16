492c3bad492d007c8fa3e859aa51bc6571c472c2
mpavicicd <mpavicicd@pmfst.hr>
2020-11-16 13:42:05 +0100

    Vjezbe_4

--- Vjezbe_4/Analyzer.cpp
+++ Vjezbe_4/Analyzer.cpp
@@ -3,72 +3,60 @@
 Analyzer::Analyzer() { //implementacija konstruktora
 }
 
-void Analyzer::readFile(){ //implementacija funkcije za ucitavanje datoteke
-	string ime1, ime2;
-	double masa1, en1, px1, py1, pz1, pT1, masa2, en2, px2, py2, pz2, pT2;
-	bool ifbozon1, ifbozon2;
-	ifstream File("Analysis.txt");
-	string line;
+void Analyzer::readFile(string fileName){ //implementacija funkcije za ucitavanje datoteke
+	ifstream File(fileName);
+	skipFirstLine = true;
 	//samo ucitavanje datoteke i ispis na ekran
-	if (File.is_open())
-	{
-	while ( getline (File,line) )
-	{
-		stringstream lineStream(line);
-		lineStream >> ime1 >> masa1 >> ifbozon1 >> en1 >> px1 >> py1 >> pz1 >> pT1 >> ime2 >> masa2 >> ifbozon2 >> en2 >> px2 >> py2 >> pz2 >>pT2;
-		cout << ime1 <<"\t"<< masa1 <<"\t"<< ifbozon1 <<"\t"<< en1 <<"\t"<< px1 <<"\t"<< py1 <<"\t"<< pz1 <<"\t"<< pT1 <<"\t"<< ime2 <<"\t"<< masa2 <<"\t"<< ifbozon2 <<"\t"<< en2 <<"\t"<< px2 <<"\t"<< py2 <<"\t"<< pz2 <<"\t"<< pT2 << endl;
+	if (File.is_open()){
+		while ( getline (File,line) ){
+			stringstream lineStream(line);
+			if(skipFirstLine){
+				skipFirstLine = false;
+				continue;
+			}
+			lineStream >> Name1 >> Mass1 >> IfBoson1 >> En1 >> px1 >> py1 >> pz1 >> pT1
+						>> Name2 >> Mass2 >> IfBoson2 >> En2 >> px2 >> py2 >> pz2 >>pT2;
+			//cout << Name1 <<"\t"<< Mass1 <<"\t"<< IfBoson1 <<"\t"<< En1 <<"\t"<< px1 <<"\t"<< py1 <<"\t"<< pz1 <<"\t"<< pT1 <<"\t"<< Name2 <<"\t"<< Mass2 <<"\t"<< IfBoson2 <<"\t"<< En2 <<"\t"<< px2 <<"\t"<< py2 <<"\t"<< pz2 <<"\t"<< pT2 << endl;
 		}
 	}
 	File.close();
 } 
 
-void Analyzer::ConvertTxtToRootFile(){
-	string	Ime1;
-	Double_t	Masa1;
-	Bool_t		IfBozon1;
-	Double_t	En1;
-	Double_t	Px1;
-	Double_t	Py1;
-	Double_t	Pz1;
-	Double_t	Pt1;
-	string	Ime2;
-	Double_t	Masa2;
-	Bool_t		IfBozon2;
-	Double_t	En2;
-	Double_t	Px2;
-	Double_t	Py2;
-	Double_t	Pz2;
-	Double_t	Pt2;
-
-	ifstream File("Analysis.txt");
-	TFile *root_file = root_file = TFile::Open("Analysis.root","RECREATE");
-
+void Analyzer::ConvertTxtToRootFile(string inputFile, TString outputFile){
+	ifstream File(inputFile);
+	skipFirstLine = true;
+	
+	//stvaranje ROOT file-a za spremanje Higgs dogadaja
+	TFile *root_file = root_file = TFile::Open(outputFile,"RECREATE");
+	//stvaranje stabla koje ce poprimiti sve Higgs dogadaje
 	TTree *tree = new TTree("Tree","Vjezbe_4");
-	tree->Branch("Ime1",&Ime1);
-	tree->Branch("Masa1",&Masa1,"Masa1/D");
-	tree->Branch("IfBozon1",&IfBozon1,"IfBozon1/O");
+	//deklaracija grana za svaku varijablu koja ce uci u stablo, postavlja se da pokazuje na pripadajucu varijablu koja ce se koristiti da puniti stablo
+	tree->Branch("Name1",&Name1);
+	tree->Branch("Mass1",&Mass1,"Mass1/D");
+	tree->Branch("IfBoson1",&IfBoson1,"IfBoson1/O");
 	tree->Branch("En1",&En1,"En1/D");
-	tree->Branch("Px1",&Px1,"Px1/D");
-	tree->Branch("Py1",&Py1,"Py1/D");
-	tree->Branch("Pz1",&Pz1,"Pz1/D");
-	tree->Branch("Pt1",&Pt1,"Pt1/D");
-	tree->Branch("Ime2",&Ime2);
-	tree->Branch("Masa2",&Masa2,"Masa2/D");
-	tree->Branch("IfBozon2",&IfBozon2,"IfBozon2/O");
+	tree->Branch("px1",&px1,"px1/D");
+	tree->Branch("py1",&py1,"py1/D");
+	tree->Branch("pz1",&pz1,"pz1/D");
+	tree->Branch("pT1",&pT1,"pT1/D");
+	tree->Branch("Name2",&Name2);
+	tree->Branch("Mass2",&Mass2,"Mass2/D");
+	tree->Branch("IfBoson2",&IfBoson2,"IfBoson2/O");
 	tree->Branch("En2",&En2,"En2/D");
-	tree->Branch("Px2",&Px2,"Px2/D");
-	tree->Branch("Py2",&Py2,"Py2/D");
-	tree->Branch("Pz2",&Pz2,"Pz2/D");
-	tree->Branch("Pt2",&Pt2,"Pt2/D");
+	tree->Branch("px2",&px2,"px2/D");
+	tree->Branch("py2",&py2,"py2/D");
+	tree->Branch("pz2",&pz2,"pz2/D");
+	tree->Branch("pT2",&pT2,"pT2/D");
   
-	string line;
-	if (File.is_open())
-	{
-	while ( getline (File,line) )
-	{
-		stringstream lineStream(line);
-		lineStream >> Ime1 >> Masa1 >> IfBozon1 >> En1 >> Px1 >> Py1 >> Pz1 >> Pt1 >> Ime2 >> Masa2 >> IfBozon2 >> En2 >> Px2 >> Py2 >> Pz2 >> Pt2;
-		tree->Fill();
+	if (File.is_open()){
+		while ( getline (File,line) ){
+			stringstream lineStream(line);
+			if(skipFirstLine){
+				skipFirstLine = false;
+				continue;
+			}
+			lineStream >> Name1 >> Mass1 >> IfBoson1 >> En1 >> px1 >> py1 >> pz1 >> pT1 >> Name2 >> Mass2 >> IfBoson2 >> En2 >> px2 >> py2 >> pz2 >> pT2;
+			tree->Fill();
 		}
 	}
 	tree->Print();
--- Vjezbe_4/Analyzer.h
+++ Vjezbe_4/Analyzer.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <fstream>
 #include <sstream>
+
 #include <TROOT.h>
 #include <TChain.h>
 #include <TFile.h>
@@ -16,6 +17,12 @@ using namespace std;
 class Analyzer{
 	public:
 		Analyzer(); //poziv konstruktora
-		void readFile();//fja za iscitavanje datoteke i ispis na konzoli
-		void ConvertTxtToRootFile();
+		void readFile(string);//fja za iscitavanje datoteke i ispis na konzoli
+		void ConvertTxtToRootFile(string, TString);
+	private:
+		string Name1, Name2;
+		double Mass1, En1, px1, py1, pz1, pT1, Mass2, En2, px2, py2, pz2, pT2;
+		bool IfBoson1, IfBoson2;
+		bool skipFirstLine;
+		string line;
 };
